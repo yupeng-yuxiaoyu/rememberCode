168fee2c2f46a95970d80c82e8f17071f46f5abc
yupeng1 <yupeng1@100tal.com>
2022-02-09 19:19:20 +0800

    feat: 二叉树左右视图,层级遍历

--- 数据结构/二叉树遍历.js
+++ 数据结构/二叉树遍历.js
@@ -36,25 +36,98 @@ var postorderTraversal = function (root, array = []) {
 */
 function levelOrderTravel(nodeTree) {
   // 如果树为空，结束
-  if(!nodeTree) return;
+  if (!nodeTree) return;
   // 初始化一个队列
   let queue = []
   // 将根节点入队
   queue.push(nodeTree)
   let node = null
   // 只要队列不为空，继续循环
-  while(queue.length) {
+  while (queue.length) {
     // 按顺序取出队列中最早入队的节点
     node = queue.shift()
     console.log(node.data);
     // 如果出队节点存在左孩子，就将其左孩子入队
-    if(node.left) {
+    if (node.left) {
       queue.push(node.left)
     }
     // 如果出队节点存在右孩子，就将其右孩子入队
-    if(node.right) {
+    if (node.right) {
       queue.push(node.right)
     }
   }
 }
 console.log("广度优先遍历-使用队列实现:");
+
+
+// 二叉树右视图
+var rightSideView = function (root) {
+  //二叉树右视图 只需要把每一层最后一个节点存储到res数组
+  let res = [],
+    queue = [];
+  queue.push(root);
+  while (queue.length && root !== null) {
+    // 记录当前层级节点个数
+    let length = queue.length;
+    while (length--) {
+      let node = queue.shift();
+      //length长度为0的时候表明到了层级最后一个节点
+      if (!length) {
+        res.push(node.val);
+      }
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+  }
+  return res;
+};
+
+// 二叉树左视图
+var leftSideView = function (root) {
+  //二叉树左视图 只需要把每一层第一个节点存储到res数组
+  let res = [],
+    queue = [];
+  queue.push(root);
+  while (queue.length && root !== null) {
+    // 记录当前层级节点个数
+    let length = queue.length;
+    while (length) {
+      let node = queue.shift();
+      //length长度为0的时候表明到了层级最后一个节点
+      if (length === queue.length) {
+        res.push(node.val);
+      }
+      length--;
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+  }
+  return res;
+};
+
+// 层序遍历
+var levelOrder = function (root) {
+  //二叉树的层序遍历
+  let res = [],
+    queue = [];
+  queue.push(root);
+  if (root === null) {
+    return res;
+  }
+  while (queue.length !== 0) {
+    // 记录当前层级节点数
+    let length = queue.length;
+    //存放每一层的节点 
+    let curLevel = [];
+    for (let i = 0; i < length; i++) {
+      let node = queue.shift();
+      curLevel.push(node.val);
+      // 存放当前层下一层的节点
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+    //把每一层的结果放到结果数组
+    res.push(curLevel);
+  }
+  return res;
+};
